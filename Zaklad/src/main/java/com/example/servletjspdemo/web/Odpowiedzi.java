0b0fe6f617a44d9fdb95845d2c802eda4e1eecd1
romety2 <lmielewczyk@sigma.ug.edu.pl>
2015-11-13 09:34:50 +0100

    Dodano serwlet

--- Zaklad/src/main/java/com/example/servletjspdemo/web/Odpowiedzi.java
+++ Zaklad/src/main/java/com/example/servletjspdemo/web/Odpowiedzi.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 public class Odpowiedzi extends HttpServlet {
 
     private long trumna = 0;
-    private long wience  = 0;
+    private long wieniec  = 0;
     private long kremacja  = 0;
     private long pochowek  = 0;
     private static final long serialVersionUID = 1L;
@@ -26,19 +26,46 @@ public class Odpowiedzi extends HttpServlet {
 
         PrintWriter out = response.getWriter();
 
+        String pogrz = "";
+
         try {
-            for (String hobby : request.getParameterValues("zaklad")) {
+            for (String zaklad : request.getParameterValues("zaklad")) {
+                pogrz = zaklad;
             }
-            out.println("<html><body><h2>Twoje dane</h2>" +
-                    "<p>Trumny: " + request.getParameter("imie") + "<br />" +
-                    "<p>Wience: " + request.getParameter("imie") + "<br />" +
-                    "<p>Kremacja zwlok: " + request.getParameter("imie") + "<br />" +
-                    "<p>Pochowek: " + request.getParameter("imie") + "<br />" +
-                    "</body></html>");
-        }
-        catch(Exception e)
-            {
-            }
+            if (pogrz.equals("trumna"))
+                trumna++;
+            else if (pogrz.equals("wieniec"))
+                wieniec++;
+            else if (pogrz.equals("kremacja"))
+                kremacja++;
+            else if (pogrz.equals("pochowek"))
+                pochowek++;
+    }
+    catch(Exception e)
+    {
+    }
+        out.println(
+                "<!DOCTYPE html>"+
+                "<html>"+
+                        "<head>"+
+                        "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">"+
+                        "<title>Ankieta</title>"+
+                        "<style rel=\"stylesheet\" type=\"text/css\">"+
+                        "body 	{background: url('czaszki.jpg');}"+
+                        "h1 	{text-align: center; color: #330033; font-size: 30px; font-style: italic;"+
+                        "text-decoration: underline ; font-family: \"Arial\"; line-height: 25px}"+
+                        ".wynik      {text-align: center; font-family: \"Arial\"}"+
+                        "</style>"+
+                        "</head>"+
+                        "<body>"+
+                        "<p class=\"link\"> <a href=\"index.jsp\">Strona glowna</a> </p>"+
+                        "<h1>Wynik</h1>" +
+                        "<p class='wynik'>Trumny: " + trumna + "<br />" +
+                        "Wience: " + wieniec + "<br />" +
+                        "Kremacja zwlok: " + kremacja + "<br />" +
+                        "Pochowek: " + pochowek + "<br /></p>" +
+                        "</body>" +
+                        "</html>");
         out.close();
     }
 }
--- Zaklad/src/main/java/com/example/servletjspdemo/web/Pytanie.java
+++ Zaklad/src/main/java/com/example/servletjspdemo/web/Pytanie.java
@@ -8,43 +8,46 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 @WebServlet(urlPatterns = "/pytanie")
 public class Pytanie extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		response.setContentType("text/html");
-		
+
 		PrintWriter out = response.getWriter();
 		out.println("<!DOCTYPE html>"+
-					"<html>"+
-						"<head>"+
-							"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">"+
-							"<title>Ankieta</title>"+
-							"<style rel=\"stylesheet\" type=\"text/css\">"+
-								"body 	{background: url('czaszki.jpg');}"+
-								"h1 	{text-align: center; color: #330033; font-size: 30px; font-style: italic;"+
-									"text-decoration: underline ; font-family: \"Arial\"; line-height: 25px}"+
-							"</style>"+
-						"</head>"+
-						"<body>"+
-							"<p class=\"link\"> <a href=\"index.jsp\">Strona glowna</a> </p>"+
-							"<h1>Jaki czynnik najbardziej wplywa na jakosc zakladu pogrzebowego</h1>" +
-							"<form action='odpowiedzi'>" +
-								"<input type='radio' name='zaklad' value='trumna'>Trumny<br />" +
-								"<input type='radio' name='zaklad' value='wieniec'>Wience<br />" +
-								"<input type='radio' name='zaklad' value='kremacja'>Kremacja zwlok<br />" +
-								"<input type='radio' name='zaklad' value='pochówek'>Pochowek<br />" +
-								"<input type='submit' value=' OK ' />" +
-							"</form>" +
-						"</body>"+
-					"</html>");
+				"<html>"+
+				"<head>"+
+				"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">"+
+				"<title>Ankieta</title>"+
+				"<style rel=\"stylesheet\" type=\"text/css\">"+
+				"body 	{background: url('czaszki.jpg');}"+
+				"h1 	{text-align: center; color: #330033; font-size: 30px; font-style: italic;"+
+				"text-decoration: underline ; font-family: \"Arial\"; line-height: 25px}"+
+				"input   {font-size: 20px}"+
+				".radio   {text-align: center; font-family: \"Arial\"}"+
+				".przycisk   {text-align: center}"+
+				"</style>"+
+				"</head>"+
+				"<body>"+
+				"<p class=\"link\"> <a href=\"index.jsp\">Strona glowna</a> </p>"+
+				"<h1>Jaki czynnik najbardziej wplywa na jakosc zakladu pogrzebowego</h1>" +
+				"<form action='odpowiedzi'>" +
+				"<p class=\"radio\"><input type='radio' name='zaklad' value='trumna'>Trumny<br />" +
+				"<input type='radio' name='zaklad' value='wieniec'>Wience<br />" +
+				"<input type='radio' name='zaklad' value='kremacja'>Kremacja zwlok<br />" +
+				"<input type='radio' name='zaklad' value='pochowek'>Pochowek<br /></p>" +
+				"<p class='przycisk'><input type='submit' value=' OK ' /></p>" +
+				"</form>" +
+				"</body>"+
+				"</html>");
 		out.close();
 	}
+
 
 }
